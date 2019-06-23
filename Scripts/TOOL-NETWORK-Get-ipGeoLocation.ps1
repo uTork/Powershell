be976e530fcd197b94e69622b0ad8fd31f58a82d
Sébastien Maltais <sebastien_maltais@hotmail.com>
2019-06-23 15:08:33 -0400

    Update TOOL-NETWORK-Get-ipGeoLocation.ps1

--- Scripts/TOOL-NETWORK-Get-ipGeoLocation.ps1
+++ Scripts/TOOL-NETWORK-Get-ipGeoLocation.ps1
@@ -1,11 +1,15 @@
 function Get-ipGeoLocation{
 <#
 .SYNOPSIS
-Get geolocation of an ip address
+Get geolocation of an ip address. You need an FREE API Key from IPSTACK https://ipstack.com/product
 .DESCRIPTION
 Use the free api from IPSTACK to resolve the location of ip address
 .PARAMETER IP
 The ip address to resolve.
+.PARAMETER APIKEY
+ FREE API Key from IPSTACK https://ipstack.com/product
+.isPowershell
+Open the isPowerShell Wiki with example on the function
 .LINK
 Sebastien Maltais
 sebastien_maltais@hotmail.com
@@ -14,14 +18,21 @@ LinkedIn: https://www.linkedin.com/in/sebastienmaltais/
 FaceBook: http://www.facebook.com/isPowerShell
 #>
 
-param([string]$IP)
+[CmdletBinding()]
 
-# free api key from ipstack https://ipstack.com/product
-$api_key = "487ba0148ea691ed95652c53f217f0b3"
+param([string]$IP,
+      [string]$APIKEY,
+      [switch]$isPowerShell
+     )
+
+if($isPowerShell -eq $true){start-process "https://github.com/uTork/Powershell/wiki/Function-%7C-Get-IPGeoLocation";break}
 
 # Api address
-$api = "http://api.ipstack.com/" + $ip + "?access_key=$api_key"
+$api = "http://api.ipstack.com/" + $ip + "?access_key=$APIKEY"
 
+# Query IPSTACK server
 $query = Invoke-RestMethod -Uri $api
+
+# output the object ip location
 $query
 }
