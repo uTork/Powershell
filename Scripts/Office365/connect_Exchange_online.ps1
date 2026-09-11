46ccb5d80a465dd70b3d358fc3527b8f578c26fb
Sébastien Maltais <sebastien_maltais@hotmail.com>
2026-09-11 13:05:36 -0400

    Remove non-language script path: Scripts/Office365/connect_Exchange_online.ps1

--- Scripts/Office365/connect_Exchange_online.ps1
+++
@@ -1,9 +0,0 @@
-$user = "sebastien.maltais@octavie.onmicrosoft.com"
-$password = ConvertTo-SecureString "888Pikac6hu!" -AsPlainText -Force
-$cred= New-Object System.Management.Automation.PSCredential ($user,$password)
-
-
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic -AllowRedirection
-Import-PSSession $Session
-
-#Remove-PSSession $Session
