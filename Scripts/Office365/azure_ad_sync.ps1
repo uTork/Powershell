5647a929ba2a3221e5045fd67bf9d9a7a0586861
Sébastien Maltais <sebastien_maltais@hotmail.com>
2026-09-11 13:05:34 -0400

    Remove non-language script path: Scripts/Office365/azure_ad_sync.ps1

--- Scripts/Office365/azure_ad_sync.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Synchronise the ad with azure after change in AD
-
-Import-Module ADSync
-
-Start-ADSyncSyncCycle -PolicyType Delta # Delta sync
-
-Start-ADSyncSyncCycle -PolicyType Initial # Full Sync
