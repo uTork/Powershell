5a3589268b12fa73a58ce46798634b198a90e606
Sébastien Maltais <sebastien_maltais@hotmail.com>
2026-09-11 12:58:31 -0400

    Add i18n scripts: Scripts/PSSession/AR/Enter_PSSession.ps1

---
+++ Scripts/PSSession/AR/Enter_PSSession.ps1
@@ -0,0 +1,7 @@
+# الاتصال بجلسة PSSession للخادم البعيد
+
+$computer = "192.168.0.169"
+$password = ConvertTo-SecureString "Pikachu888!" -AsPlainText -Force
+$cred= New-Object System.Management.Automation.PSCredential ("administrator",$password)
+
+Enter-PSSession -ComputerName $computer -Credential $cred
