a9502bf6d253f40b0e164549fb5b3113be76b610
Sébastien Maltais <sebastien_maltais@hotmail.com>
2026-09-11 12:58:39 -0400

    Add i18n scripts: Scripts/PSSession/ES/Enter_PSSession.ps1

---
+++ Scripts/PSSession/ES/Enter_PSSession.ps1
@@ -0,0 +1,7 @@
+# Conectar a la PSSession del servidor remoto
+
+$computer = "192.168.0.169"
+$password = ConvertTo-SecureString "Pikachu888!" -AsPlainText -Force
+$cred= New-Object System.Management.Automation.PSCredential ("administrator",$password)
+
+Enter-PSSession -ComputerName $computer -Credential $cred
