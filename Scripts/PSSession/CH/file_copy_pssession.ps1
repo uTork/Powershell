ef37d5edc785403ae2181d68d4812f48059fcfa9
Sébastien Maltais <sebastien_maltais@hotmail.com>
2026-09-11 12:58:35 -0400

    Add i18n scripts: Scripts/PSSession/CH/file_copy_pssession.ps1

---
+++ Scripts/PSSession/CH/file_copy_pssession.ps1
@@ -0,0 +1,10 @@
+# 将文件和文件夹复制到远程 PSSession
+
+$computer = "192.168.0.169"
+$password = ConvertTo-SecureString "Pikachu888!" -AsPlainText -Force
+$cred= New-Object System.Management.Automation.PSCredential ("administrator",$password)
+
+
+$TargetSession = New-PSSession -ComputerName $computer -Credential $cred
+
+Copy-Item -ToSession $TargetSession -Path "C:\power\users.txt" -Destination "C:\log" -Recurse
