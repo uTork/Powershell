81bc772db1e67e83e6c3dff44cf1fd1d8f4e3919
Sébastien Maltais <sebastien_maltais@hotmail.com>
2026-09-11 12:55:39 -0400

    Add i18n scripts: Scripts/EDR/UK/TOOL-EDR-Reporting.ps1

---
+++ Scripts/EDR/UK/TOOL-EDR-Reporting.ps1
@@ -0,0 +1,27 @@
+# Цей скрипт отримує статус EDR в аркуші Excel електронною поштою.
+# Потрібні модулі: Active Directory та ImportExcel
+# WinRM увімкнено на кожному сервері/комп’ютері та потрібні права адміністратора для запуску Powershell ISE
+
+
+$server_list = (get-adcomputer -filter * -Properties * | where-object {$_.OperatingSystem -like "*server*"}).name
+
+$liste = @(
+foreach($srv in $server_list){
+
+$ping = Test-Connection -Count 2 -ComputerName $srv -Quiet
+
+if($ping -eq $true){
+
+
+                    Invoke-Command -computername $srv -scriptblock {Get-MpComputerStatus -ErrorAction SilentlyContinue} -ErrorAction SilentlyContinue
+                   
+
+                   }
+
+}
+)
+
+
+$liste | Export-Excel -Path "C:\script\report_EDR_Server_status.xlsx" -WorksheetName "EDR" -TableName "EDR" -TableStyle Medium9 -AutoSize
+
+Send-MailMessage -Attachments "C:\script\report_EDR_Server_status.xlsx" -SmtpServer x.x.x.x -From EDR_Servers_Status@contose.com -to "sebastien.maltais@contoso.com" -Subject "Rapport Server EDR"
